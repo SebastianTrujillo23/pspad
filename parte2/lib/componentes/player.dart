07c8683703757e540d3ef83dde991bd1a2b3f68d
Denis de la Vieja Gonzalez <denis.delavieja@gmail.com>
2025-02-21 12:13:33 +0100

    implementacion del salto, gravedad y animaciones

--- parte2/lib/componentes/player.dart
+++ parte2/lib/componentes/player.dart
@@ -1,46 +1,98 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flutter/services.dart';
 
 import '../CoinGame.dart';
 
-enum PlayerState { idle, running }
-
-enum PlayerDirection { left, right, none}
+enum PlayerState { idle, running, jumping, falling }
 
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<CoinGame> {
+    with HasGameRef<CoinGame>, KeyboardHandler,  CollisionCallbacks {
   String character;
-  Player({position, required this.character}): super(position: position);
+  Player({
+    position,
+    this.character = 'Ninja Frog',
+  }) : super(position: position);
 
+  final double stepTime = 0.05;
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
-  final double stepTime = 0.05;
+  late final SpriteAnimation jumpingAnimation;
+  late final SpriteAnimation fallingAnimation;
 
-  PlayerDirection playerDirection = PlayerDirection.left;
+  final double _gravity = 9;
+  final double _jumpForce = 270;
+  final double _terminalVelocity = 300;
+  double horizontalMovement = 0;
   double moveSpeed = 100;
   Vector2 velocity = Vector2.zero();
+  bool isOnGround = false;
+  bool hasJumped = false;
+  List<CollisionBlock> collisionBlocks = [];
+  CustomHitbox  hitbox = CustomHitbox (
+    offsetX: 10,
+    offsetY: 4,
+    width: 14,
+    height: 28,
+  );
 
   @override
   FutureOr<void> onLoad() {
     _loadAllAnimations();
+    // debugMode = true;
+    add(RectangleHitbox(
+        position: Vector2(hitbox.offsetX, hitbox.offsetY),
+        size: Vector2(hitbox.width, hitbox.height)
+    ));
     return super.onLoad();
   }
 
   @override
   void update(double dt) {
     _updatePlayerMovement(dt);
+    _updatePlayerState();
+    _checkHorizontalCollisions();
+    _apllyGravity(dt);
+    _checkVerticalCollisions();
     super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent( KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    horizontalMovement = 0;
+    final isLeftkeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRightkeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    horizontalMovement += isLeftkeyPressed ? -1 : 0;
+    horizontalMovement += isRightkeyPressed ? 1 : 0;
+
+    hasJumped = keysPressed.contains(LogicalKeyboardKey.space);
+
+    return super.onKeyEvent(event, keysPressed);
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (other is Fruit) other.collidedWithPlayer();
+    super.onCollision(intersectionPoints, other);
   }
 
   void _loadAllAnimations() {
     idleAnimation = _spriteAnimation('Idle', 11);
     runningAnimation = _spriteAnimation('Run', 12);
+    jumpingAnimation = _spriteAnimation('Jump', 1);
+    fallingAnimation = _spriteAnimation('Fall', 1);
 
     // List of all animations
     animations = {
       PlayerState.idle: idleAnimation,
       PlayerState.running: runningAnimation,
+      PlayerState.jumping: jumpingAnimation,
+      PlayerState.falling: fallingAnimation,
     };
 
     // Set current animation
@@ -58,21 +110,94 @@ class Player extends SpriteAnimationGroupComponent
     );
   }
 
-  void _updatePlayerMovement(double dt) {
-    double dirX = 0.0;
-    switch (PlayerDirection){
-      case PlayerDirection.left:
-        dirX -= moveSpeed;
-        break;
-      case PlayerDirection.right:
-        dirX += moveSpeed;
-        break;
-      case PlayerDirection.none:
-        break;
-      default:
+  void _updatePlayerState() {
+    PlayerState playerState = PlayerState.idle;
+
+    if(velocity.x < 0 && scale.x > 0) {
+      flipHorizontallyAroundCenter();
+    } else if (horizontalMovement > 0 && scale.x < 0) {
+      flipHorizontallyAroundCenter();
     }
 
-    velocity = Vector2(dirX, 0.0);
-    position += velocity;
+    // Check if moving, set running
+    if (velocity.x > 0 || velocity.x < 0) playerState = PlayerState.running;
+
+    // Check if falling, set to falling
+    if(velocity.y > 0) playerState = PlayerState.falling;
+
+    // Checks if jumping, set to jumping
+    if (velocity.y < 0) playerState = PlayerState.jumping;
+
+    current = playerState;
+  }
+
+  void _updatePlayerMovement(double dt) {
+    if(hasJumped && isOnGround) _playerJump(dt);
+
+    // if (velocity.y > _gravity) isOnGround = false; // Esto es opcional
+
+    velocity.x = horizontalMovement * moveSpeed;
+    position.x += velocity.x * dt;
+  }
+
+  void _playerJump(double dt) {
+    velocity.y = -_jumpForce;
+    position.y += velocity.y * dt;
+    isOnGround = false;
+    hasJumped = false;
+  }
+
+  void _checkHorizontalCollisions() {
+    for (final block in collisionBlocks) {
+      if (!block.isPlatform) {
+        if (checkCollision(this, block)) {
+          if (velocity.x > 0) {
+            velocity.x = 0;
+            position.x = block.x - hitbox.offsetX - hitbox.width;
+            break;
+          }
+          if (velocity.x < 0) {
+            velocity.x = 0;
+            position.x = block.x + block.width + hitbox.width + hitbox.offsetX;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  void _apllyGravity(double dt) {
+    velocity.y += _gravity;
+    velocity.y = velocity.y.clamp(-_jumpForce, _terminalVelocity);
+    position.y += velocity.y * dt;
+  }
+
+  void _checkVerticalCollisions() {
+    for (final block in collisionBlocks) {
+      if (block.isPlatform) {
+        if (checkCollision(this, block)) {
+          if (velocity.y > 0) {
+            velocity.y = 0;
+            position.y = block.y - hitbox.height - hitbox.offsetY;
+            isOnGround = true;
+            break;
+          }
+        }
+      } else {
+        if (checkCollision(this, block)) {
+          if (velocity.y > 0) {
+            velocity.y = 0;
+            position.y = block.y - hitbox.height - hitbox.offsetY;
+            isOnGround = true;
+            break;
+          }
+          if (velocity.y < 0) {
+            velocity.y = 0;
+            position.y = block.y + block.height - hitbox.offsetY;
+          }
+        }
+      }
+    }
   }
 }
+
