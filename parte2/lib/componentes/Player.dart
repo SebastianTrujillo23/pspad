3b4d75fbc6688949c083cdc6e64793fb8fc54b48
Denis de la Vieja Gonzalez <denis.delavieja@gmail.com>
2025-02-21 11:39:53 +0100

    creacion del Player

---
+++ parte2/lib/componentes/Player.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+
+import '../CoinGame.dart';
+
+enum PlayerState { idle, running }
+
+class Player extends SpriteAnimationGroupComponent with HasGameRef<CoinGame>{
+  late final SpriteAnimation idleAnimation;
+  final double stepTime = 0.05;
+
+
+  @override
+  FutureOr<void> onLoad() {
+    _loadAllAnimations();
+    return super.onLoad();
+  }
+
+  void _loadAllAnimations(){
+    idleAnimation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('Main Characters/Ninja Frog/Idle (32x32).png'),
+      SpriteAnimationData.sequenced(
+        amount: 11,
+        stepTime: stepTime,
+        textureSize: Vector2.all(32),
+      ),
+    );
+
+    animations = {
+
+    };
+  }
+
+}
