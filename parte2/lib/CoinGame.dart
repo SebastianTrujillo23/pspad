e22eb08211dfc042d23d8344573dc28aeb9a5dea
Denis de la Vieja Gonzalez <denis.delavieja@gmail.com>
2025-02-21 11:55:35 +0100

    mejora del movimiento del personaje

--- parte2/lib/CoinGame.dart
+++ parte2/lib/CoinGame.dart
@@ -8,26 +8,50 @@ import 'dart:ui';
 
 import 'componentes/level.dart';
 
-class CoinGame extends FlameGame {
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/painting.dart';
+
+import 'componentes/player.dart';
+  class CoinGame extends FlameGame
+  with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
   late final CameraComponent cam;
 
-  final world = Level();
+  final world = Level(levelName: 'Level-01');
+  Player player = Player(character: 'Mask Dude');
+  //late JoystickComponent joystick;
+  //bool showJoystick = false;
 
   @override
-  FutureOr<void> onLoad() {
-    Future<void> onLoad() async {
-      // Cargar todas las imagenes dentro del cache
-      await images.loadAllImages();
+  Future<void> onLoad() async {
+  // Cargar todas las imagenes dentro del cache
+  await images.loadAllImages();
 
-      cam = CameraComponent.withFixedResolution(
-          world: world, width: 640, height: 360);
-      cam.viewfinder.anchor = Anchor.topLeft;
+  final world = Level(
+  player: player,
+  levelName: 'Level-01',
+  );
 
-      addAll([cam, world]);
+  cam = CameraComponent.withFixedResolution(
+  world: world, width: 640, height: 360);
+  cam.viewfinder.anchor = Anchor.topLeft;
 
-      return super.onLoad();
-    }
+  addAll([cam, world]);
+
+  //if (showJoystick) {
+  //addJoystick();
+  //}
+
+  return super.onLoad();
   }
-}
+
+  //@override
+
+  }
