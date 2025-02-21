bd37034bd7b4bb5d7b27f7474d4b2071a13ea870
Denis de la Vieja Gonzalez <denis.delavieja@gmail.com>
2025-02-21 11:53:17 +0100

    incorporacion movimiento del jugador

--- parte2/lib/componentes/player.dart
+++ parte2/lib/componentes/player.dart
@@ -1,52 +1,78 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
-import '../CoinGame.dart';
-import 'dart:async';
 
-import 'package:flame/components.dart';
+import '../CoinGame.dart';
 
 enum PlayerState { idle, running }
-  class Player extends SpriteAnimationGroupComponent
-  with HasGameRef<CoinGame> {
+
+enum PlayerDirection { left, right, none}
+
+class Player extends SpriteAnimationGroupComponent
+    with HasGameRef<CoinGame> {
   String character;
-  Player({required this.character});
+  Player({position, required this.character}): super(position: position);
 
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
   final double stepTime = 0.05;
 
+  PlayerDirection playerDirection = PlayerDirection.left;
+  double moveSpeed = 100;
+  Vector2 velocity = Vector2.zero();
 
   @override
   FutureOr<void> onLoad() {
-  _loadAllAnimations();
-  return super.onLoad();
+    _loadAllAnimations();
+    return super.onLoad();
   }
 
+  @override
+  void update(double dt) {
+    _updatePlayerMovement(dt);
+    super.update(dt);
+  }
 
   void _loadAllAnimations() {
-  idleAnimation = _spriteAnimation('Idle', 11);
-  runningAnimation = _spriteAnimation('Run', 12);
+    idleAnimation = _spriteAnimation('Idle', 11);
+    runningAnimation = _spriteAnimation('Run', 12);
 
-  // List of all animations
-  animations = {
-
-  PlayerState.idle: idleAnimation,
-  PlayerState.running: runningAnimation,
-  };
+    // List of all animations
+    animations = {
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation,
+    };
 
     // Set current animation
     current = PlayerState.idle;
   }
 
   SpriteAnimation _spriteAnimation(String state, int amount) {
-  return SpriteAnimation.fromFrameData(
+    return SpriteAnimation.fromFrameData(
       game.images.fromCache('Main Characters/$character/$state (32x32).png'),
       SpriteAnimationData.sequenced(
-          amount: amount,
-          stepTime: stepTime,
-          textureSize: Vector2.all(32),
-        ),
-      );
-    }
+        amount: amount,
+        stepTime: stepTime,
+        textureSize: Vector2.all(32),
+      ),
+    );
   }
+
+  void _updatePlayerMovement(double dt) {
+    double dirX = 0.0;
+    switch (PlayerDirection){
+      case PlayerDirection.left:
+        dirX -= moveSpeed;
+        break;
+      case PlayerDirection.right:
+        dirX += moveSpeed;
+        break;
+      case PlayerDirection.none:
+        break;
+      default:
+    }
+
+    velocity = Vector2(dirX, 0.0);
+    position += velocity;
+  }
+}
