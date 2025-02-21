760e4e6130fda5969cceb9670cf804fe40122e8f
Denis de la Vieja Gonzalez <denis.delavieja@gmail.com>
2025-02-21 11:52:01 +0100

    incorporacion animaciones

--- parte2/lib/componentes/player.dart
+++ parte2/lib/componentes/player.dart
@@ -1,35 +1,52 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
-
 import '../CoinGame.dart';
+import 'dart:async';
+
+import 'package:flame/components.dart';
 
 enum PlayerState { idle, running }
+  class Player extends SpriteAnimationGroupComponent
+  with HasGameRef<CoinGame> {
+  String character;
+  Player({required this.character});
 
-class Player extends SpriteAnimationGroupComponent with HasGameRef<CoinGame>{
   late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runningAnimation;
   final double stepTime = 0.05;
 
 
   @override
   FutureOr<void> onLoad() {
-    _loadAllAnimations();
-    return super.onLoad();
+  _loadAllAnimations();
+  return super.onLoad();
   }
 
-  void _loadAllAnimations(){
-    idleAnimation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('Main Characters/Ninja Frog/Idle (32x32).png'),
+
+  void _loadAllAnimations() {
+  idleAnimation = _spriteAnimation('Idle', 11);
+  runningAnimation = _spriteAnimation('Run', 12);
+
+  // List of all animations
+  animations = {
+
+  PlayerState.idle: idleAnimation,
+  PlayerState.running: runningAnimation,
+  };
+
+    // Set current animation
+    current = PlayerState.idle;
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
+  return SpriteAnimation.fromFrameData(
+      game.images.fromCache('Main Characters/$character/$state (32x32).png'),
       SpriteAnimationData.sequenced(
-        amount: 11,
-        stepTime: stepTime,
-        textureSize: Vector2.all(32),
-      ),
-    );
-
-    animations = {
-
-    };
+          amount: amount,
+          stepTime: stepTime,
+          textureSize: Vector2.all(32),
+        ),
+      );
+    }
   }
-
-}
