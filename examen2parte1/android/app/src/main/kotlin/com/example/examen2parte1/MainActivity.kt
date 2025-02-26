0ce62bb4b85e850fb0a7ef867efa365530be251f
Denis de la Vieja Gonzalez <denis.delavieja@gmail.com>
2025-02-26 09:44:22 +0100

    push

--- examen2parte1/android/app/src/main/kotlin/com/example/examen2parte1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.examen2parte1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- parte2/android/app/src/main/kotlin/com/example/parte2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.parte2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
